be7e23be09f29c797b1e66356551626266463126
Teke Byas <tekelamon01@gmail.com>
2023-03-27 18:42:17 -0700

    first half of class examples

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 document.getElementById('executeBtn').addEventListener('click', function() {
     const outputElement = document.getElementById('output');
 
+/* JavaScript data types:
+      - string
+      - number
+      - boolean
+      - object
+      - function
+*/
+
     // Your JavaScript code goes here
+
+// string examples
     const message = 'Hello, JavaScript!';
-    outputElement.textContent = message;
+    let firstName = "Teke";
+    let lastName = "Byas";
+    console.log( firstName + ' ' + lastName );
+
+// number examples
+    let grossIncome = 90000;
+    let tax = 0.05;
+    let netIncome = grossIncome - ( grossIncome * tax );
+
+    console.log( "$" + netIncome );
+
+// boolean examples
+    console.log("1" === 1);
+    console.log("1" == 1);
+    console.log( 3 > 5 );
+    console.log( true );
+    console.log( false );
+
+    outputElement.textContent = firstName + ' ' + lastName;
 });
